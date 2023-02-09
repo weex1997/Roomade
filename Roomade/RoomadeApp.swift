e19b7abc3c772a4063c10b07b07c44d57b6d61ff
wee <87364144+weex1997@users.noreply.github.com>
2023-02-09 09:21:51 +0300

    Initial Commit

---
+++ Roomade/RoomadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomadeApp.swift
+//  Roomade
+//
+//  Created by Wedad Almehmadi on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RoomadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
